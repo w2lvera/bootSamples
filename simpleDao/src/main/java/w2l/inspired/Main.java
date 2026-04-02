e111782f5e47b3f5aedaa0cee41a03e60ec30478
w2lvera <w2lvera@gmail.com>
2026-04-02 22:21:48 +0300

    crud_jdbcTemplate write daily log to daily_logs

--- simpleDao/src/main/java/w2l/inspired/Main.java
+++ simpleDao/src/main/java/w2l/inspired/Main.java
@@ -2,11 +2,6 @@ package w2l.inspired;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import w2l.inspired.dao.CustomerDao;
-import w2l.inspired.dao.CustomerSimpleDao;
-import w2l.inspired.logical.CustomersChecker;
 
 @SpringBootApplication
 public class Main {
