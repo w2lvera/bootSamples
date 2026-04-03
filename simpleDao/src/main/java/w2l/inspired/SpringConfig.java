852575beb83f0a609b4e13a35a57fd16c263a78f
w2lvera <w2lvera@gmail.com>
2026-04-03 17:15:27 +0300

    crud_jdbcTemplate add liquibase for data migration

--- simpleDao/src/main/java/w2l/inspired/SpringConfig.java
+++ simpleDao/src/main/java/w2l/inspired/SpringConfig.java
@@ -1,9 +1,12 @@
 package w2l.inspired;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
@@ -17,6 +20,14 @@ public class SpringConfig {
         // Порядок важен, если есть другие шаблонизаторы (например, Thymeleaf)
         bean.setOrder(1);
         return bean;
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:database/changelog.xml");
+        liquibase.setDataSource(dataSource);
+        return liquibase;
     }
 
 }
---
+++ simpleDao/src/main/resources/database/changeset-1.sql
@@ -0,0 +1,43 @@
+-- drops
+DROP TABLE IF EXISTS public.customers;
+
+DROP SEQUENCE IF EXISTS public.customers_id;
+
+DROP TABLE IF EXISTS public.daily_logs;
+
+DROP SEQUENCE IF EXISTS public.logs_id;
+
+-- creates
+
+CREATE SEQUENCE public.customers_id
+    INCREMENT 1
+    START WITH 1;
+
+CREATE TABLE IF NOT EXISTS public.customers
+(
+    id integer NOT NULL DEFAULT nextval('customers_id'),
+    name varchar NOT NULL,
+    CONSTRAINT customers_pkey PRIMARY KEY (id)
+);
+
+CREATE SEQUENCE IF NOT EXISTS public.logs_id
+    INCREMENT 1
+    START WITH 1;
+
+
+CREATE TABLE IF NOT EXISTS public.daily_logs
+(
+    id integer NOT NULL DEFAULT nextval('logs_id'),
+    date date NOT NULL DEFAULT CURRENT_DATE,
+    customer_id integer NOT NULL,
+    CONSTRAINT daily_logs_pkey PRIMARY KEY (id),
+    CONSTRAINT daily_logs_customers_fkey
+          FOREIGN KEY(customer_id)
+    	  REFERENCES public.customers(id)
+    	  ON DELETE CASCADE
+);
+
+insert into customers(name) values ('Ivanov');
+insert into customers(name) values ('Petrov');
+insert into customers(name) values ('Sidorov');
+
